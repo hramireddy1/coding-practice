99be0a2f0b6a89621a795a4f154408f62cdf7db5
suryatejabandlamudi <suryatejabandlamudi@gmail.com>
2020-07-01 21:55:14 +0530

    got error when changed 'long' to 'int' in line 5

--- 01-07-2020/arranging_coins_surya.cpp
+++ 01-07-2020/arranging_coins_surya.cpp
@@ -1,1 +1,17 @@
-// test
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long current_level_sum = 0;
+        long current_level = 0;
+        
+        while(current_level_sum<n)
+        {
+            current_level += 1;
+            current_level_sum  = (current_level*(current_level+1))/2;
+            if(current_level_sum>n)
+                return current_level -1;
+                
+        }
+        return int(current_level);
+    }
+};
